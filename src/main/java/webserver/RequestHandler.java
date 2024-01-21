a2dd57b77ae3db20721ae57de063fdecb8bba569
RH SIM <tlafidgy@naver.com>
2024-01-21 21:24:12 +0900

    1. index.html complete

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,21 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.HashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.User;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -24,10 +32,42 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
+        	BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        	String line = br.readLine();
+        	System.out.println("----line----:"+line);
+        	if(line == null) {
+        		return;
+        	}
+        	
+        	String url = HttpRequestUtils.getURL(line);
+        	System.out.println("1--------URL : "+url);
+        	
+        	if(url.startsWith("/user/create")){
+        		System.out.println("2--------URL : "+url);
+        		HashMap<String,String> map = new HashMap<String,String>();
+        		String[] splited = url.split("\\?");
+        		String path = splited[1];
+        		String[] data = path.split("&");
+        		
+        		
+            	for(int i=0; i< data.length; i++) {
+            		String[] key = data[i].split("=");
+            		map.put(key[0], key[1]);
+            		
+            	}
+            	User us = new User(map.get("userId"),map.get("password"),map.get("name"),map.get("email"));
+            	System.out.println(us.toString());
+        		
+        		url = "/index.html";
+        	}
+        	
+            			
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
